eaeddf5203013d1c5aa6c13ed3394f21b10ee1eb
ziyichen <ziyichen@huashenghaoche.com>
2019-03-10 12:47:48 +0800

    重置洗衣机

--- app/src/main/java/com/lotus/lotusapp/C09Activity.java
+++ app/src/main/java/com/lotus/lotusapp/C09Activity.java
@@ -808,6 +808,7 @@ public class C09Activity extends Activity {
         builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+                washCommand = new ArrayList<>();
                 return;
             }
         });
